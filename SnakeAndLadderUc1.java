62948dea077cc332159998337e956a469da5ee0d
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-07-17 12:29:08 +0530

    Added:Start_Position_Zero

---
+++ SnakeAndLadderUc1.java
@@ -0,0 +1,16 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderUc1 {
+		public static  int source = 0 ;
+		public static  int destination = 100 ;
+		public static void main(String[] args) {
+			 
+				int player = source ;
+				int player2=destination;
+				System.out.println("here the player start");
+				System.out.println("Player start Position is:" + player);
+				System.out.println();
+				System.out.println("here the player end");
+				System.out.println("Player end position is:"+ player2);
+		}
+	}
